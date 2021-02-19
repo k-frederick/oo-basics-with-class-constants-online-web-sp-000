b128034c42badb235314bcae6a4f5f612690b1cc
Kristina Frederick <kristinafrederick@icloud.com>
2021-02-19 22:04:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,33 +1,21 @@
-# class Shoe
-#   attr_accessor :color, :size, :material, :condition
-#   attr_reader :brands
-#
-#   BRANDS = []
-#
-#   def initialize(brand)
-#     @brand = brand
-#   end
-#
-#   def cobble
-#     self.condition = "new"
-#     puts "Your shoe is as good as new!"
-#   end
-#   def brand=(brand)
-#     @brand = brand
-#     BRANDS << brand
-#   end
-# end
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brands
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+     BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
 end
+
